45d07d4fd31840303693c8eb5e48fa74ae2afcc0
MarcosSilva2021 <mfsilva5585@gmail.com>
2025-09-14 21:00:24 -0300

    First Commit

---
+++ src/main/java/com/mars/getcep/GetcepApplication.java
@@ -0,0 +1,13 @@
+package com.mars.getcep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetcepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetcepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mars/getcep/GetcepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mars.getcep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetcepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
